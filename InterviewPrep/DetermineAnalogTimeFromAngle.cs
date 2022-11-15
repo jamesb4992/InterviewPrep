fda98554d01f9e3cc54b5e4717741653570c0e77
JAMES BIRAPAKA <jamesb4992@gmail.com>
2022-11-15 09:39:31 -0500

    clean up

--- InterviewPrep/DetermineAnalogTimeFromAngle.cs
+++ InterviewPrep/DetermineAnalogTimeFromAngle.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace InterviewPrep
+﻿namespace InterviewPrep
 {
     internal class DetermineAnalogTimeFromAngle
     {
-        public static void determineAnalogTime(int angle)
+        internal static void DetermineAnalogTime(int angle)
         {
 
             Func<int, string> time = (angle) => $"{angle / 30} : {angle % 30 * 2}";
