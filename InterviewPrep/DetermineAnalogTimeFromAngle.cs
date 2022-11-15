5c97356b15b8268d81c2bb8e211aa89c9fd8eeb2
James Birapaka <James Birapaka@DESKTOP-0QSP85Q>
2022-11-15 09:36:56 -0500

    Add project files.

---
+++ InterviewPrep/DetermineAnalogTimeFromAngle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPrep
+{
+    internal class DetermineAnalogTimeFromAngle
+    {
+        public static void determineAnalogTime(int angle)
+        {
+
+            Func<int, string> time = (angle) => $"{angle / 30} : {angle % 30 * 2}";
+            Console.WriteLine(time(angle));
+        }
+    }
+}
---
+++ InterviewPrep/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//for(int i = 0;  i<=360; i++)
+//{
+//    new AnalogTime().determineAnalogTime(i);
+//}
+
+
+//public class AnalogTime
+//{
+//    public void determineAnalogTime(int angle)
+//    {
+
+//       Func<int, string> time = (angle) => $"{angle / 30} : {angle % 30 * 2}";
+//       Console.WriteLine(time(angle));
+//    }
+//
+
+
+using InterviewPrep;
+
+FindMaxConsecutiveOnes.FindMaxConsecutiveOnesInBinaryArray(FindMaxConsecutiveOnes.binaryArray);
